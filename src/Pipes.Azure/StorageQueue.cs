5aaebfe4e5f0827cc3a69582858cbcf0e811708d
Fabricio Catae <fcatae@microsoft.com>
2018-05-18 16:39:17 -0300

    implement queue

---
+++ src/Pipes.Azure/StorageQueue.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.WindowsAzure.Storage.Queue;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pipes.Azure
+{
+    public class StorageQueue
+    {
+        class Message
+        {
+            public readonly CloudQueueMessage InternalMessage;
+
+            public Message(CloudQueueMessage message)
+            {
+                this.InternalMessage = message;
+            }
+
+            public string Content => InternalMessage.AsString;
+        }
+
+        private readonly CloudQueue _queue;
+
+        public StorageQueue(string queueUrl)
+        {
+            this._queue = new CloudQueue(new Uri(queueUrl));
+        }
+
+        Message TryReadQueue()
+        {
+            var message = _queue.GetMessageAsync().Result;
+
+            return (message != null) ? new Message(message) : null;
+        }
+
+        void DeleteMessage(Message message)
+        {
+            _queue.DeleteMessageAsync(message.InternalMessage);
+        }
+
+        public string Read()
+        {
+            const int BACKOFF_INITIAL = 500;
+            const int BACKOFF_THRESHOLD = 60000;
+            const int BACKOFF_MULTIPLIER = 2;
+
+            var message = TryReadQueue();
+            int backoff = BACKOFF_INITIAL;
+
+            while(message == null)
+            {
+                Task.Delay(backoff).Wait();
+
+                if (backoff < BACKOFF_THRESHOLD)
+                {
+                    backoff *= BACKOFF_MULTIPLIER;
+                }
+
+                message = TryReadQueue();
+            }
+
+            string content = message.Content;
+
+            DeleteMessage(message);
+
+            return content;
+        }
+    }
+}
---
+++ src/ReadQueue/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Pipes.Azure;
+
+namespace ReadQueue
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            if (args.Length == 0)
+                return -1;
+
+            string storageUrl = args[0];
+
+            var queue = new StorageQueue(storageUrl);
+            
+            while(true)
+            {
+                var message = queue.Read();
+                Console.WriteLine(message);
+            }
+        }
+    }
+}
