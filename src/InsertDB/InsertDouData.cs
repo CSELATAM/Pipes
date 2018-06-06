e9d0be0a1d50defa76d2ed91a0a6490711401859
Fabricio Catae <fcatae@microsoft.com>
2018-06-06 14:14:26 -0300

    Retrieve article ID

--- src/InsertDB/InsertDouData.cs
+++ src/InsertDB/InsertDouData.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Text;
 using System.Xml;
 using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace InsertDB
 {
     class InsertDouData
     {
+        static Regex ArticleIdRegex = new Regex(@"-artigo(\d+)");
+
         public ExecSqlCmd CreateInput(string data)
         {
             string command = CreateInputCommand();
@@ -18,7 +21,7 @@ namespace InsertDB
         
         string CreateInputCommand()
         {
-            return "INSERT tbLog(id) VALUES(@Param1)";
+            return "INSERT tbArtigos(Filename, PubName, PubDate, ArtigoId, Hierarquia, Assinaturas) VALUES( @Filename, @PubName, @PubDate, @ArtigoId, @Hierarquia, @Assinaturas)";
         }
 
         object CreateInputModel(string input)
@@ -26,24 +29,29 @@ namespace InsertDB
             var doc = new XmlDocument();
             doc.LoadXml(input);
 
-            string name = doc.SelectSingleNode("xml/@name")?.Value;
+            string name = doc.SelectSingleNode("xml/@filename")?.Value;
             string pubName = doc.SelectSingleNode("xml/article/@pubName").Value;
             string pubDate = doc.SelectSingleNode("xml/article/@pubDate").Value;
             string artCategory = doc.SelectSingleNode("xml/article/@artCategory").Value;
             string identifica = doc.SelectSingleNode("xml/article/body/Identifica").InnerText;
 
+            string filename = name.Split('/').Last();
             string[] hierarquia = artCategory.Split('/');
             string[] assinaturas = doc.SelectNodes("xml/article/body/Autores/assina")
                                     .Cast<XmlNode>()
                                     .Select(x => x.InnerText)
                                     .ToArray();
 
+            var artigoMatch = ArticleIdRegex.Match(name);
+            int artigoId = Int32.Parse(artigoMatch.Groups[1].Value );
+
             return new { 
-                Filename = name,
+                Filename = filename,
                 PubName = pubName,
                 PubDate = pubDate,
-                Hierarquia = hierarquia,
-                Assinaturas = assinaturas
+                ArtigoId = artigoId,
+                Hierarquia = String.Join('/', hierarquia),
+                Assinaturas = String.Join('/', assinaturas)
             };
         }
     }
